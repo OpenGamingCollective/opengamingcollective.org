e0b80f072ce2cd2711afb90e9ef564d1a71d5746
Cypress Reed <willow@willowidk.dev>
2026-07-24 21:59:47 -0600

    format

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,40 +1,40 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
-const isCI = !!process.env['CI'];
+const isCI = !!process.env["CI"];
 
 export default defineConfig({
-	forbidOnly: isCI,
-	fullyParallel: true,
-	projects: [
-		{
-			name: 'Chrome',
-			use: {
-				...devices['Desktop Chrome'],
-				channel: isCI ? 'chrome' : undefined,
-				headless: true
-			}
-		},
-		{
-			name: 'Mobile Chrome',
-			use: {
-				...devices['Pixel 5'],
-				channel: isCI ? 'chrome' : undefined,
-				headless: true
-			}
-		}
-	],
-	reporter: 'list',
-	testDir: './tests',
-	testMatch: /.*\.test\.ts/,
-	// The timeout for the accessibility tests only.
-	timeout: 180 * 1_000,
-	webServer: [
-		{
-			command: 'pnpm run preview:test',
-			reuseExistingServer: !isCI,
-			// The timeout of the single build step ran before the accessibility tests.
-			timeout: 120 * 1_000,
-			url: 'http://localhost:8787'
-		}
-	]
+  forbidOnly: isCI,
+  fullyParallel: true,
+  projects: [
+    {
+      name: "Chrome",
+      use: {
+        ...devices["Desktop Chrome"],
+        channel: isCI ? "chrome" : undefined,
+        headless: true,
+      },
+    },
+    {
+      name: "Mobile Chrome",
+      use: {
+        ...devices["Pixel 5"],
+        channel: isCI ? "chrome" : undefined,
+        headless: true,
+      },
+    },
+  ],
+  reporter: "list",
+  testDir: "./tests",
+  testMatch: /.*\.test\.ts/,
+  // The timeout for the accessibility tests only.
+  timeout: 180 * 1_000,
+  webServer: [
+    {
+      command: "pnpm run preview:test",
+      reuseExistingServer: !isCI,
+      // The timeout of the single build step ran before the accessibility tests.
+      timeout: 120 * 1_000,
+      url: "http://localhost:8787",
+    },
+  ],
 });
